42b4fa6354c1afb4bb9466e212702cbaaf871bbd
badrulamin69 <badrulaminidb69@gmail.com>
2026-02-01 19:06:33 +0600

    ;[jhj

---
+++ LearnMethod/src/learnmethod/LearnMethod.java
@@ -0,0 +1,34 @@
+
+package learnmethod;
+
+import java.util.Scanner;
+
+
+public class LearnMethod {
+
+ 
+    public static void main(String[] args) {
+//       Scanner input = new Scanner(System.in)
+//              
+//    }
+//    
+//    public static int checkFactorialNumber(int number){
+//    
+//    int i=1;
+//    int count =1;
+//    
+//    for(int i=1; i <= number; i++){
+//        count * = i;
+//   
+//    }
+//    
+//    return count;
+//    }
+
+    public static int doSum(int... n1){
+        
+    }
+    
+    
+    
+}
