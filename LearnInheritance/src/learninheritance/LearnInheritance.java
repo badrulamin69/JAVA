19383cf67010ce4c101a88b96c47b24197b7707d
badrulamin69 <badrulaminidb69@gmail.com>
2026-02-09 19:14:39 +0600

    add

---
+++ LearnInheritance/src/learninheritance/LearnInheritance.java
@@ -0,0 +1,22 @@
+
+package learninheritance;
+
+import learninheritance.mobile.Android;
+
+
+public class LearnInheritance {
+
+   
+    public static void main(String[] args) {
+        
+        Android android = new Android();
+        
+        android.setBrower("Chrome");
+        android.setSetOs("android");
+        android.setPlayStore("yes");
+        android.setTouchScreen("Yes");
+        
+
+    }
+    
+}
---
+++ LearnInheritance/src/learninheritance/mobile/Apple.java
@@ -0,0 +1,17 @@
+
+package learninheritance.mobile;
+
+public class Apple extends Mobile{
+    
+    Private String facecall;
+    Private String appstore;
+    Private String imessage;
+    
+    
+}
+
+
+
+    
+ 
+   
