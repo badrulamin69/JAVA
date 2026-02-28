5206f093098803cba3fb91092b7ba81673ceab62
badrulamin69 <badrulaminidb69@gmail.com>
2026-02-28 15:56:21 +0600

    add

---
+++ BinaryIO/src/binaryio/BinaryIO.java
@@ -0,0 +1,42 @@
+package binaryio;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BinaryIO {
+    
+    public static void main(String[] args) {
+        
+        try {
+            FileOutputStream output = new FileOutputStream("F:\\JAVA Git\\JAVA\\BinaryIO.dat");
+            for (int i = 1; i <= 10; i++) {
+                output.write(i);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BinaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BinaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        try {
+            FileInputStream input = new FileInputStream("F:\\JAVA Git\\JAVA\\BinaryIO.dat");
+            
+            int value;
+            while ((value = input.read()) != -1){
+                System.out.println(value + " ");
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BinaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BinaryIO.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+    }
+}
