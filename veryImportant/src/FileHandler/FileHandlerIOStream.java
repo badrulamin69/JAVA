3b651bcd1c121981ecf1bfc337acbc6c74b1d476
Badrul Amin <badrulamin.cu.bd@gmail.com>
2026-03-03 01:08:17 +0600

    add

--- veryImportant/src/FileHandler/FileHandlerIOStream.java
+++ veryImportant/src/FileHandler/FileHandlerIOStream.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FileHandler;
 
-/**
- *
- * @author hp
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class FileHandlerIOStream {
-    
+
+    public static void main(String[] args) {
+
+        File f = new File(".//emon.dat");
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            
+            for(int i=1; i<=10; i++){
+                fos.write(i);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileHandlerIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHandlerIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            
+            int a;
+            while((a = fis.read()) != -1){
+                System.out.println(a);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileHandlerIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHandlerIOStream.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
