b979e43440f17d4d620d55df1cb9ff271c3b97f8
Badrul Amin <badrulaminidb69@gmail.com>
2026-04-11 18:00:31 +0600

    add

--- CrudEmployee/src/crudemployee/CrudEmployee.java
+++ CrudEmployee/src/crudemployee/CrudEmployee.java
@@ -5,10 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class CrudEmployee {
-
-
+    
     public static void main(String[] args) {
         
     }
@@ -35,5 +33,6 @@ public class CrudEmployee {
          ps = getCon().prepareStatement(updateSql);
     
     }
+    
     
 }
